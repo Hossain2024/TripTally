7e1eb5e52606d4cf88104f25e27e488bb387212c
Maliha <hossainmaliha18@gmail.com>
2025-04-23 22:24:14 -0700

    initialized

--- TripTally/ViewController.swift
+++ TripTally/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    let budgetKey = "userBudget"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let savedBudget = UserDefaults.standard.string(forKey: budgetKey) {
+        currencyLabel.text = savedBudget
+            
+        }
+        
     }
-
-
+    
+    @IBAction func editButtonTapped(_ sender: UIButton) {
+        let alert = UIAlertController(title: "set Budget", message:"Enter your Budget for this trip", preferredStyle:.alert)
+        
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enter amount"
+            textField.keyboardType = .decimalPad
+        }
+        
+        
+        let saveAction = UIAlertAction(title: "OK", style: .default) { [weak self] _ in
+            if let text = alert.textFields?.first?.text,
+                let amount = Double(text) {
+                        
+                let formatted = String(format: "$%.2f", amount)
+                self?.currencyLabel.text = formatted
+                        
+                        // Save to UserDefaults
+                UserDefaults.standard.set(formatted, forKey: self?.budgetKey ?? "")
+                
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+            
+    }
+    
+    
 }
 
